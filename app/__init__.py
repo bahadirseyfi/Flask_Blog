c3033296ba40b56b8b04527fb5488aebd4027f9e
bahadirseyfi <bahadirsoftware@gmail.com>
2020-11-18 17:08:36 +0300

    Package Structure 1

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask,render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '34f88ce590a9039ce3aa652f74b24c15'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
